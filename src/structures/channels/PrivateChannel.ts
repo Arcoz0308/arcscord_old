844852dcba95152eef234f53732e05472b9fb099
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2021-06-09 13:13:00 +0100

    add createMessage to PrivateChannel (with other user)

--- src/structures/channels/PrivateChannel.ts
+++ src/structures/channels/PrivateChannel.ts
@@ -2,4 +2,16 @@ import { Channel } from './Channel';
 
 
 export class PrivateChannel extends Channel {
+  
+  public createMessage(content: string, msg?: MessageOptions): Promise<Message>;
+  public createMessage(msg: MessageOptionsWithContent): Promise<Message>;
+  public async createMessage(
+    cOrM: string | MessageOptionsWithContent,
+    msg?: MessageOptions
+  ): Promise<Message> {
+    if (typeof cOrM === "string")
+      return this.client.createMessage(this.id, cOrM, msg);
+    return this.client.createMessage(this.id, cOrM);
+  }
+  
 }
